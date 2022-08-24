c552caa3f5791e61a942b878e6b0273ebd9fedf4
Aholicknight <zuckonit@riseup.net>
2022-08-24 12:50:46 -0600

    Update socks.py

--- socks.py
+++ socks.py
@@ -60,7 +60,7 @@ from errno import EOPNOTSUPP, EINVAL, EAGAIN
 from io import BytesIO
 from os import SEEK_CUR
 import collections
-collections.callable = collections.abc.callable # inputerror fix
+collections.callable = collections.abc # inputerror fix
 from base64 import b64encode
 
 PROXY_TYPE_SOCKS4 = SOCKS4 = 1
